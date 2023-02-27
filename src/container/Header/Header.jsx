673f3bb31a6a789d3d211f11cada0d6af69da296
Rishabh <rishabh.9agarwal@gmail.com>
2023-02-27 14:14:56 +0530

    style changes

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -68,7 +68,7 @@ const Header = () => {
     <motion.div
       whileInView={{ opacity: [0, 1] }}
       transition={{ duration: 0.5, delayChildren: 0.5 }}
-      className="app__header-img"
+      className="app__header-img app__flex"
     >
       {/* <img src={images.circle_logo} alt="profile_bg" /> */}
       <motion.img
